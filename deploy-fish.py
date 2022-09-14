aac5e050a28ff2fab6169021bc92f53a19c83520
liusen <yjghdsss@163.com>
2022-09-14 23:43:07 +0800

    start up

--- deploy-fish.py
+++ deploy-fish.py
@@ -1,4 +1,5 @@
 import subprocess
+from colorama import Fore
 
 
 # tmux 结构
@@ -16,7 +17,7 @@ import subprocess
 #             pane-2: coin-distribution
 #             pane-3: 
 #         }
-#         window-middleware { // 运行 redis, mysql, rabbitmq 等中间件
+#         window-middleware { // 运行 redis, mysql, rabbitmq 等服务
 #             pane-0: redis
 #             pane-1
 #         }
@@ -32,8 +33,9 @@ WIN_SERVICE = "service"
 WIN_MINER = "miner"
 
 # 项目路径
-POOL_DIR = ""
-COIN_DIR = ""
+POOL_DIR = "/home/node/data/pool/fish"
+GATE_DIR = "/home/node/data/pool/gate"
+COIN_DIR = "/home/node/data/pool/distribution"
 
 
 def run_cmd(cmd: str):
@@ -66,15 +68,18 @@ def build_tmux():
     pool_tmux()
     middleware_tmux()
     node_tmux()
+    print("tmux built")
 
 
 def middleware_tmux():
     cmds = [
         f"tmux new-window -t {SESSION_FISH}:1 -n {WIN_SERVICE}",
+        f'tmux rename-window -t {SESSION_FISH}:1 {WIN_SERVICE}'
         f"tmux splitw -h -p 50",
     ]
     for cmd in cmds:
         run_cmd(cmd)
+    print("middleware sevices tmux built")
 
 
 def node_tmux():
@@ -85,6 +90,7 @@ def node_tmux():
     ]
     for cmd in cmds:
         run_cmd(cmd)
+    print("node-tmux built")
 
 
 def pool_tmux():
@@ -95,35 +101,67 @@ def pool_tmux():
         f"tmux splitw -v -p 70",
         f"tmux selectp -t 0",
         f"tmux splitw -v -p 50",
+        f'tmux rename-window -t {SESSION_FISH}:0 {WIN_POOL}'
     ]
 
     for cmd in cmds:
         run_cmd(cmd)
+    print("pool-tmux built")
 
 
-def run_pool():
+def run_pool_modules():
+    print("running pool modules")
     # run pool
     cmds = [
         f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cd {POOL_DIR}" C-m',
-        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cargo run fish-pool --release" C-m',
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "fish-pool" C-m',
     ]
     for cmd in cmds:
         run_cmd(cmd)
+    print("pool started")
 
     # run pool-gate
     cmds = [
-        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cd {POOL_DIR}" C-m',
-        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cargo run pool-gate --release" C-m',
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.1 "cd {GATE_DIR}" C-m',
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.1 "pool-gate" C-m',
     ]
     for cmd in cmds:
         run_cmd(cmd)
+    print("pool gate started")
 
     # run coin distribution
     cmds = [
-        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cd {COIN_DIR}" C-m',
-        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.0 "cargo run --release" C-m',
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.2 "cd {COIN_DIR}" C-m',
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_POOL}.2 "coin-distribution" C-m',
     ]
     for cmd in cmds:
         run_cmd(cmd)
-
+    print("coin distribution started")
     
+def run_node():
+    node_cmd = 'ironfish start -v --rpc.tcp.host=127.0.0.1 --rpc.tcp.port=14444 --rpc.tcp.secure --rpc.tcp'
+    cmds = [
+        f'tmux send-keys -t {SESSION_NODE}:{WIN_NODE}.0 "{node_cmd}" C-m',
+    ]
+    for cmd in cmds:
+        run_cmd(cmd)
+    print(Fore.GREEN + "node started")
+    
+def run_services():
+    redis_cmd = 'redis-server'
+    cmds = [
+        f'tmux send-keys -t {SESSION_FISH}:{WIN_SERVICE}.0 "{redis_cmd}" C-m',
+    ]
+    for cmd in cmds:
+        run_cmd(cmd)
+    print(Fore.GREEN + "redis started")
+    
+
+if __name__ == "__main__":
+    kill_session(SESSION_FISH)
+    kill_session(SESSION_NODE)
+
+    build_tmux()
+    run_pool_modules()
+    run_node()
+    run_services()
